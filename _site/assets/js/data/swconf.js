e9e99221bb131363d4edd5573da83f77a0d78cbe
AnDDoanf <thuanan24@gmail.com>
2024-09-27 23:13:15 +0700

    modify post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723913726',resources: [
+    cacheName: 'chirpy-1727453553',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
